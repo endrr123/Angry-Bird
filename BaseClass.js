6c8b8cf2fac3f6cc4973653816e76cc86e9eb30e
endrr123 <vatsal,tewari123@gmail.com>
2021-01-09 12:03:44 +0700

    second commit

---
+++ BaseClass.js
@@ -0,0 +1,21 @@
+class BaseClass {
+    constructor(x,y,width,height,angle) {
+        var options = {'restitution': 0.4, 'friction': 4, 'density': 2.2};
+        this.body = Bodies.rectangle(x,y,20,height,options)
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("sprites/base.png")
+        World.add(world,this.body);
+     }
+
+     display() {
+         var angle = this.body.angle;
+         push();
+         translate(this.body.position.x,this.body.position.y);
+         rotate(angle);
+         imageMode(CENTER);
+         image(this.image,0,0,this.width, this.height);
+         pop();
+         
+     }
+}
