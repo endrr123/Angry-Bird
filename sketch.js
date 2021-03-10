0c9958fb214fee21ef6062a9940ac316f87b0f5b
endrr123 <vatsal.tewari123@gmail.com>
2021-03-10 11:51:48 +0700

    updated Slingshot.js

--- sketch.js
+++ sketch.js
@@ -1,69 +1,74 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, box2, box3, box4, box5;
-var ground, platform;
-var pig1,pig2;
-var log1, log2, log3, log4, log5;
-var bird;
-var backgroundImage;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
 
-function preload(){
-  backgroundImage = loadImage("sprites/bg.png");
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
-function setup() {
-  createCanvas(1200, 400);
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  engine = Engine.create();
-  world = engine.world;
 
-  ground = new Ground(600, height, 1200, 20);
-  platform = new Ground(150,305,300,170);
-  log5 = new Log(230, 180, 80, PI / 2);
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
-  box1 = new Box(700, 320, 70, 70);
-  box2 = new Box(920, 320, 70, 70);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-  pig1 = new Pig(810, 350);
-  
-  log1 = new Log(810, 260, 300, PI / 2);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
 
-  box3 = new Box(700, 240, 70, 70);
-  box4 = new Box(920, 240, 70, 70);
-  pig2 = new Pig(810, 220);
-  log2 = new Log(810, 180, 300, PI / 2);
+    log3 =  new Log(810,180,300, PI/2);
 
-  box5 = new Box(810, 160, 70, 70);
-  log3 = new Log(760, 120, 150, PI / 7);
-  log4 = new Log(870, 120, 150, -PI / 7);
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
 
-  bird = new Bird(100, 100);
-  chain = new Chain(bird.body,log5.body)
+    bird = new Bird(100,100);
+
+    slingShot = new Slingshot(bird.body,{x:200, y:100});
 }
 
-function draw() {
-  background(backgroundImage);
-  Engine.update(engine);
-  box1.display();
-  box2.display();
-  ground.display();
-  pig1.display();
-  log1.display();
-  box3.display();
-  box4.display()
-  pig2.display();
-  log2.display();
-  box5.display();
-  log3.display();
-  log4.display();
-  bird.display();
-  platform.display();
-  log5.display();
-  chain.display();
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
 
-  
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    slingShot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x: mouseX, y: mouseY});
+}
+
+function mouseReleased(){
+    slingShot.fly()
 }
